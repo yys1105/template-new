c6fc0e838684cf7409db6b71d95e583497fe7319
yao <yao@qq.com>
2018-01-26 00:43:49 +0800

    feat:实现一部分功能

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,17 +1,106 @@
 <script>
   import phone from './phone'
+
   export default {
     name: '',
-    components:{phone},
+    components: {phone},
     data() {
       return {
-        form:{
-          title:''
-        }
+        title: '大',
+        sections: [
+          {
+            title: {
+              type: '0',
+              value: '小'
+            },
+            contents: [{
+              type: '0',
+              value: '内'
+            }]
+          }
+        ],
+        titleOptions: [{
+          value: '0',
+          label: '无特殊样式'
+        }, {
+          value: '1',
+          label: '绿色标题'
+        }, {
+          value: '2',
+          label: '红色标题'
+        }, {
+          value: '3',
+          label: '一级大标题'
+        }],
+        contentOptions: [{
+          value: '0',
+          label: '无特殊样式'
+        }, {
+          value: '1',
+          label: '备注类型'
+        }],
+        dom:''
       }
     },
-    methods:{
-
+    methods: {
+      addDiv(item) {
+        item.contents.push({
+          type: '0',
+          value: ''
+        })
+      },
+      deleteDiv(item,index){
+        item.contents = [].concat(item.contents.slice(0,index),item.contents.slice(index+1,item.contents.length))
+      },
+      addSection(){
+        this.sections.push({
+          title: {
+            type: '0',
+            value: ''
+          },
+          contents: [{
+            type: '0',
+            value: ''
+          }]
+        })
+      },
+      createElement(){
+        var preImgPath = '/static/images'
+        var dom = ''
+        dom = `<div class="card">
+                <div class="header">
+                  <div class="icon">
+                    <img src="${preImgPath}/rep_bullet.png">
+                  </div>
+                  <div class="title">
+                    ${this.title}
+                  </div>
+                  </div>${createCardElement(this.sections,dom)}</div>`
+        console.log(dom)
+        this.dom += dom
+        function createCardElement(sections,sectionsElementString) {
+          for(var i of sections){
+            var divString = ''
+            divString = createDivElement(i.contents,divString)
+            sectionsElementString+=`<div class="content">
+                                      <div class="section">
+                                        <div class="title">${i.title.value}</div>
+                                        ${divString}
+                                      </div>
+                                    </div>`
+          }
+          return sectionsElementString
+        }
+        function createDivElement(values,DivsElementString) {
+          for(var i of values){
+            DivsElementString+=`<div>${i.value}</div>`
+          }
+          return DivsElementString
+        }
+      },
+      recall(){
+        this.$refs.phone.recall()
+      }
     }
   }
 </script>
@@ -19,14 +108,50 @@
 <template>
   <div class="home">
     <div class="preview">
-      <phone></phone>
+      <phone :dom="dom" ref="phone"></phone>
     </div>
     <div class="edit">
-      <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="标题">
-          <el-input v-model="form.title"></el-input>
-        </el-form-item>
-      </el-form>
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <el-input v-model="title" placeholder="请输入内容"></el-input>
+        </div>
+        <div>
+          <div class="section" v-for="item in sections">
+            <div class="label">
+              <el-select class="select" size="small" v-model="item.title.type" placeholder="请选择">
+                <el-option
+                  v-for="item in titleOptions"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+              <el-input class="input" v-model="item.title.value" size="small" placeholder="请输入段落标题"></el-input>
+            </div>
+            <div class="label" v-for="(div,key,index) in item.contents">
+              <el-select class="select" size="large" v-model="div.type" placeholder="请选择">
+                <el-option
+                  v-for="item in contentOptions"
+                  :key="item.value"
+                  :label="item.label"
+                  :value="item.value">
+                </el-option>
+              </el-select>
+              <el-input v-model="div.value" placeholder="请输入段落内容"></el-input>
+              <el-button type="danger" icon="el-icon-circle-close" plain @click="deleteDiv(item,key)"> </el-button>
+            </div>
+            <div class="add-button" @click="addDiv(item)">+点击增加一个div</div>
+          </div>
+          <div class="section">
+            <div class="add-button" @click="addSection">+点击增加一个段落</div>
+          </div>
+        </div>
+        <div class="button-wrap">
+          <el-button type="primary" @click="createElement">立即创建</el-button>
+          <el-button>清空</el-button>
+          <el-button type="warning" @click="recall">撤回</el-button>
+        </div>
+      </el-card>
     </div>
   </div>
 </template>
@@ -34,10 +159,33 @@
 <style scoped lang="stylus">
   .home
     display flex
+    height 100%
     .edit
       flex 1
+      height 100%
+      overflow auto
     .preview
       width 375px
       height 667px
+      overflow auto
       background-color #2a2955
+
+  .label
+    display flex
+
+  .section + .section
+    margin-top 20px
+    padding-top 20px
+    border-top 2px dashed #409EFF
+
+  .add-button
+    border-radius 8px
+    padding 8px
+    border 1px dashed #409EFF
+    text-align center
+    color #409EFF
+    cursor pointer
+    font-size 14px
+  .button-wrap
+    padding 8px
 </style>
--- src/pages/phone.vue
+++ src/pages/phone.vue
@@ -2,23 +2,249 @@
   export default {
     name: '',
     props:{
-      data:{
-        type:Array
+      dom:{
+        type:String
+      }
+    },
+    watch:{
+      dom:function(val,oldVal){
+        if(val!==oldVal){
+          this.oldDom.push(oldVal)
+        }
+        var vdom = document.getElementById("dom")
+        vdom.innerHTML = val
       }
     },
     data() {
-      return {}
+      return {
+        oldDom:[],
+        recallIndex:0
+      }
+    },
+    methods:{
+      recall(){
+        this.dom = this.oldDom.reverse().slice(this.recallIndex,this.recallIndex+1)[0]
+      }
     }
   }
 </script>
 
 <template>
-  <div>
-    ddd
+  <div class="bg-color">
+    <div class="main-content">
+      <div class="card wel-card">
+        <div class="content">
+          <div class="section wel-words">
+            尊敬的××（先生/女士）：
+          </div>
+          <div class="section">
+            您好！十分荣幸您选择我们为康基因公司为您进行本次基因检测服务！
+          </div>
+          <div class="section">
+            我们很高兴为您提供此次检测的个性化基因检测报告，本检测对相关疾病的发生风险进行全面评估，由专业遗传学专家对您的疾病风险及生活习惯给予详细的解读和相应的健康指导，本次检测所涉及的DNA来自您最新提供的采集样本，检测内容有专业科学依据。本检测利用目前行业内先进的基因检测技术，准确获取相关位点基因型，从基因水平上对疾病的发生进行早期预测。
+          </div>
+          <div class="section">
+            基于您的基因检测信息，我们提供个性化健康指导：
+            <div>一、疾病预测——在健康或亚健康时能准确预测患病易感风险</div>
+            <div>二、疾病预防——主动调节外因（饮食/运动/生活习惯/环境），降低患病风险</div>
+            <div>三、健康管理——优化常规检查方案，提供自身健康管理方案</div>
+          </div>
+          <div class="section">
+            根据世界卫生组织（WHO）报告，通过健康的生活方式，如戒烟、戒酒、运动和均衡饮食习惯以及有效运用当今的医疗手段（如定期筛检和早期检测），至少有50%以上的疾病是可以预防或避免的。针对相关疾病高风险的人群，我们会建议您加强对相关疾病的关注和认知，尽早开展相关健康检查，将患病风险控制在最低水平。
+          </div>
+          <div class="section">
+            鉴于疾病发生机理的复杂性，当前医学对某些疾病的遗传基础认识的局限性及疾病的个体差异，我们不排除某些受检者有漏检的易感基因或致病变异位点。本检测报告仅供参考，不做临床诊断意见。
+          </div>
+        </div>
+      </div>
+      <div id="dom">
+
+      </div>
+    </div>
   </div>
 </template>
 
 
-<style scoped>
+<style>
+  * {
+    padding: 0;
+    margin: 0;
+  }
+  html {
+    line-height: initial;
+    font-size: 100px;
+  }
+  body {
+    font-size: 0.14rem;
+  }
+  .bg-color{
+    background-color: #2a2955;
+  }
+  .main-content{
+    padding: 0 .09rem .25rem .09rem;
+  }
+  .card{
+    border-radius: .04rem;
+    background-color: #fff;
+  }
+  .wel-card{
+    background-color: #06a7f8;
+  }
+  .card{
+    margin-top: .25rem;
+  }
+  .card>.header{
+    display: flex;
+    align-items: center;
+    position: relative;
+    height: .6rem;
+    padding: 0 .19rem;
+  }
+  .card>.header:after{
+    content:'';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: #dbdbdb;
+    height: 1px;
+    transform-origin: 0 0;
+    transform: scaleY(.5);
+  }
+  .card>.header .icon{
+    height: .3rem;
+    margin-right: .12rem;
+  }
+  .card>.header .icon>img{
+    height: 100%;
+  }
+  .card>.header .title{
+    font-size: .16rem;
+    color: #06a8f4;
+  }
+  .card>.content{
+    padding: .18rem;
+  }
+  .card>.table-content{
+    padding: .09rem;
+  }
+  .section{
+    font-size: .14rem;
+    color: #333;
+    word-wrap: break-word;
+  }
+  .wel-card .section{
+    color: #fff;
+  }
+  .section+.section{
+    margin-top: .1rem;
+  }
+  .section .title{
+    color: #c48b66;
+  }
+  .section .title.red{
+    color: #dd4433;
+  }
+  .section .title.green{
+    color: #56c847;
+  }
+  .tip{
+    display: flex;
+    align-items: center;
+    height: .45rem;
+  }
+  .tip>.icon{
+    height: .16rem;
+    margin-right: .09rem;
+  }
+  .tip>.icon>img{
+    height: 100%;
+  }
+  .tip>.text{
+    font-size: .16rem;
+    color: #838587;
+  }
+  .table{
+    width: 100%;
+  }
+  .table tr th{
+    font-size: .12rem;
+    color: #c69777;
+    min-width: .55rem;
+    height: .5rem;
+    text-align: center;
+  }
+  .table tr td{
+    font-size: .12rem;
+    color: #2d2f31;
+    height: .5rem;
+    text-align: center;
+    border-top: 1px dashed #dbdbdb;
+  }
+  .check-more {
+    display: none;
+    border-top: 1px dashed #dbdbdb;
+    height: .5rem;
+    line-height: .5rem;
+    text-align: center;
+    font-size: .14rem;
+    color: #c48b66;
+  }
+  .card .footer{
+    height: .5rem;
+    padding: 0 .16rem;
+    display: flex;
+    align-items: center;
+    position: relative;
+  }
+  .card .footer:before{
+    content:'';
+    position: absolute;
+    top: 0;
+    left: .08rem;
+    right: .08rem;
+    border-top: 2px dotted #dbdbdb;
+    transform-origin: 0 0;
+    transform: scaleY(.5);
+  }
+  .card .footer .icon{
+    height: .14rem;
+  }
+  .card .footer .icon>img{
+    height: 100%;
+  }
+  .card .footer .question-link{
+    height: .5rem;
+    line-height: .5rem;
+    flex: 1;
+    text-align: center;
+    font-size: .14rem;
+    color: #c48b66;
+  }
+  .wel-card .section{
+    text-indent: .28rem;
+  }
+  .wel-card .section div{
+    text-indent: 0!important;
+  }
+  .wel-words{
+    font-size: .18rem;
+    padding-top: .15rem;
+    padding-bottom: .05rem;
+    text-indent: 0!important;
+  }
+  .remark{
+    font-size: .13rem;
+    color: #b5b5b5;
+  }
+  .light{
+    font-size: .13rem;
+    /*color: #818181;*/
+  }
+  .h1{
+    /*font-size: .15rem;*/
+    font-weight: 400;
+    color: #000;
+  }
 
 </style>
